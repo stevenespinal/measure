69c403d3b80b900a99820700b9134da20eb2bb3b
Steven Espinal <stevenjesusespinal@gmail.com>
2019-07-21 13:17:47 -0400

    book appointment button added

--- models/users.js
+++ models/users.js
@@ -1,60 +1,60 @@
 module.exports = function(sequelize, DataTypes) {
-    var User = sequelize.define("User", {
-        name: {
-            type: DataTypes.STRING,
-            required: true,
-            validate: {
-                len: [1]
-            }
-        },
-        phone: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        email: {
-            type: DataTypes.STRING,
-            isUnique: true,
-            allowNull: false,
-            validate: {
-                len: [1],
-                isEmail: true
-            }
-        },
-        address: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        city: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        state: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
-                len: [1]
-            }
-        },
-        zipCode: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            validate: {
-                len: [5]
-            }
-        },
-        bookingConfirmation: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    });
-    return User;
-}
+  var User = sequelize.define("User", {
+    name: {
+      type: DataTypes.STRING,
+      required: true,
+      validate: {
+        len: [1]
+      }
+    },
+    phone: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      isUnique: true,
+      allowNull: false,
+      validate: {
+        len: [1],
+        isEmail: true
+      }
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    city: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    state: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1]
+      }
+    },
+    zipCode: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      validate: {
+        len: [5]
+      }
+    },
+    bookingConfirmation: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  return User;
+};
