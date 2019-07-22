fdb49f047a09f95f558d675ea4777d378a4c248b
Kimberly <kamueller2@gmail.com>
2019-07-21 20:24:56 -0400

    survey

--- mentors.js
+++ mentors.js
@@ -1,51 +1,82 @@
-module.exports = [{
-    name: "Elizabeth Easley, APRN, BC, PMHCNS--BC",
-    bio: "Experienced Psychiatric Nurse with a history of working in the mental health field. Strong background in the delivery of psychiatric services including over thirty years of complex medication management. Highly skilled in all areas of mental health care including Mindfulness, Nursing Education, Crisis Intervention, Family, Individual and Group Therapy.",
-    phone: 555888888,
-    email: "eeasley@measure.com"
-}, {
-    name: "Vicki Spitzer, MSN, ARNP, CNS",
-    bio: "Experienced Psychiatric Nurse with a Post-Master's Rush University, College of Nursing. Expert knowledge in anxiety disorders, chronic mental illness, mood disorders, psychotic disorders.",
-    phone: 555888888,
-    email: "vspitzer@measure.com"
-}, {
-    name: "Abby Summers, MSN, ARNP, CNS",
-    bio: "Ceritfied APRN with exper knowledge in various areas, such as Attention Deficit Hyperactivity Disorder, Cognitive Impairment/Dementia, Culture.",
-    phone: 555888888,
-    email: "asummers@measure.com"
-}, {
-    name: "Jeff Stein, MD, PhD, FRCPsych",
-    bio: "Chief of the Mood Brain & Development Unit at the National Institute of Mental Health. Dr Stein's aim is to understand why some young people become more depressed than others and how to improve our understanding and treatment of depression.",
-    phone: 555888888,
-    email: "jstein@measure.com"
-}, {
-    name: "Ken Duckworth, PhD",
-    bio: "Associate Professor in the Couple and Family Therapy Doctoral Program in the Department of Human Development and Family Studies at Morgan State University.",
-    phone: 555888888,
-    email: "kduckworth@measure.com"
-}, {
-    name: "Andrea Meyer, PhD",
-    bio: "AAAMFT Clinical Fellow, Licensed Marriage and Family Therapist, and Licensed Professional Counselor who has been in part-time private practice since 1988.",
-    phone: 555888888,
-    email: "ameyer@measure.com"
-}, {
-    name: "Brian Rush, PhD",
-    bio: "AAAMFT Clinical Fellow, Licensed Marriage and Family Therapist, and Licensed Professional Counselor who has been in part-time private practice since 1988.",
-    phone: 555888888,
-    email: "brush@measure.com"
-}, {
-    name: "Tim R Peterson, Ph.D.",
-    bio: "Professor of Cognitive Neuroscience at Purdue University. Experienced in behavioral, attention and cognitive development",
-    phone: 555888888,
-    email: "tpeterson@measure.com"
-}, {
-    name: "Zachary Pincus, M.D., Ph.D.",
-    bio: "Neuroscientist with a vast knowledge in autism, face perception, visual attention, psychophysics, social cognition, comparative cognition",
-    phone: 555888888,
-    email: "zpincus@measure.com"
-}, {
-    name: "Matt Rose, Ph.D.",
-    bio: "Areas of experties include mentoring relationships, risk and protective factors in adolescent development, emerging adulthood, preventive interventions",
-    phone: 555888888,
-    email: "mrose@measure.com"
-}];
+module.exports = [
+  {
+  name: "Elizabeth Easley, APRN, BC, PMHCNS--BC",
+  bio:
+      "Experienced Psychiatric Nurse with a history of working in the mental health field. Strong background in the delivery of psychiatric services including over thirty years of complex medication management. Highly skilled in all areas of mental health care including Mindfulness, Nursing Education, Crisis Intervention, Family, Individual and Group Therapy.",
+  phone: 555888888,
+  email: "eeasley@measure.com",
+  scores: [1, 2, 3, 4]
+  },
+  {
+  name: "Vicki Spitzer, MSN, ARNP, CNS",
+  bio:
+      "Experienced Psychiatric Nurse with a Post-Master's Rush University, College of Nursing. Expert knowledge in anxiety disorders, chronic mental illness, mood disorders, psychotic disorders.",
+  phone: 555888888,
+  email: "vspitzer@measure.com",
+  scores: [3, 2, 1, 4]
+  },
+  {
+  name: "Abby Summers, MSN, ARNP, CNS",
+  bio:
+      "Ceritfied APRN with exper knowledge in various areas, such as Attention Deficit Hyperactivity Disorder, Cognitive Impairment/Dementia, Culture.",
+  phone: 555888888,
+  email: "asummers@measure.com",
+  scores: [4, 3, 2, 1]
+  },
+  {
+  name: "Jeff Stein, MD, PhD, FRCPsych",
+  bio:
+      "Chief of the Mood Brain & Development Unit at the National Institute of Mental Health. Dr Stein's aim is to understand why some young people become more depressed than others and how to improve our understanding and treatment of depression.",
+  phone: 555888888,
+  email: "jstein@measure.com",
+  scores: [1, 1, 1, 1]
+  },
+  {
+  name: "Ken Duckworth, PhD",
+  bio:
+      "Associate Professor in the Couple and Family Therapy Doctoral Program in the Department of Human Development and Family Studies at Morgan State University.",
+  phone: 555888888,
+  email: "kduckworth@measure.com",
+  scores: [1, 2, 2, 4]
+  },
+  {
+  name: "Andrea Meyer, PhD",
+  bio:
+      "AAAMFT Clinical Fellow, Licensed Marriage and Family Therapist, and Licensed Professional Counselor who has been in part-time private practice since 1988.",
+  phone: 555888888,
+  email: "ameyer@measure.com",
+  scores: [3, 2, 4, 4]
+  },
+  {
+  name: "Brian Rush, PhD",
+  bio:
+      "AAAMFT Clinical Fellow, Licensed Marriage and Family Therapist, and Licensed Professional Counselor who has been in part-time private practice since 1988.",
+  phone: 555888888,
+  email: "brush@measure.com",
+  scores: [2, 3, 4, 1]
+  },
+  {
+  name: "Tim R Peterson, Ph.D.",
+  bio:
+      "Professor of Cognitive Neuroscience at Purdue University. Experienced in behavioral, attention and cognitive development",
+  phone: 555888888,
+  email: "tpeterson@measure.com",
+  scores: [3, 4, 1, 2]
+  },
+  {
+  name: "Zachary Pincus, M.D., Ph.D.",
+  bio:
+      "Neuroscientist with a vast knowledge in autism, face perception, visual attention, psychophysics, social cognition, comparative cognition",
+  phone: 555888888,
+  email: "zpincus@measure.com",
+  scores: [4, 4, 4, 4]
+  },
+  {
+  name: "Matt Rose, Ph.D.",
+  bio:
+      "Areas of experties include mentoring relationships, risk and protective factors in adolescent development, emerging adulthood, preventive interventions",
+  phone: 555888888,
+  email: "mrose@measure.com",
+  scores: [3, 3, 3, 1]
+  }
+];
